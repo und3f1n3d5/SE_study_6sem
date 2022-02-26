127ce671131695c881dd8b94073e04e63d678807
und3f1n3d5 <55402266+und3f1n3d5@users.noreply.github.com>
2022-02-26 13:54:17 +0300

    Update M3i.h

--- pavliuchenkov_d_m/prj.lab/m3i/M3i.h
+++ pavliuchenkov_d_m/prj.lab/m3i/M3i.h
@@ -12,21 +12,28 @@
 //template <typename T>
 class M3i {
 public:
-    explicit M3i(const uint64_t w=1, const uint64_t h=1, const uint64_t d=1, int64_t* data=nullptr);
+    explicit M3i(uint64_t w=1, uint64_t h=1, uint64_t d=1, int64_t* data=nullptr);
     M3i(const M3i &right);
     M3i(M3i &&right) noexcept = default;
 
     M3i &operator=(const M3i &right);
     M3i &operator=(M3i &&right) noexcept = default;
 
-    int64_t GetElement(const uint64_t x=0, const uint64_t y=0, const uint64_t z=0);
-    void SetElement(const int64_t element, const uint64_t x=0, const uint64_t y=0, const uint64_t z=0);
+    int64_t at(uint64_t x=0, uint64_t y=0, uint64_t z=0) const;
+    int64_t &at(uint64_t x=0, uint64_t y=0, uint64_t z=0);
+    int64_t GetWidth() const;
+    int64_t GetHeight() const;
+    int64_t GetDepth() const;
+    void SetElement(int64_t element, uint64_t x=0, uint64_t y=0, uint64_t z=0);
 
-    void Resize(const uint64_t w=1, const uint64_t h=1, const uint64_t d=1);
+    void Resize(uint64_t w=1, uint64_t h=1, uint64_t d=1);
     uint64_t GetSize() const;
 
     M3i copy();
     M3i clone();
+
+    void Fill(int64_t element=0);
+    void SetDefault(int64_t element=0);
 
     ~M3i();
 
@@ -39,8 +46,10 @@ private:
 
     static void Check(M3i &r);
 
-    void ConstructFromPointer(int64_t* ptr, const uint64_t w=1, const uint64_t h=1, const uint64_t d=1);
+    void ConstructFromPointer(int64_t* ptr, uint64_t w=1, uint64_t h=1, uint64_t d=1);
 };
 
+std::ostream& operator << (std::ostream &os, const M3i &a);
+std::istream& operator >> (std::istream &in, M3i &a);
 
 #endif //SE_STUDY_PAVLIUCHENKOV_M3I_H
