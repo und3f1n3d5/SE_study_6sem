2cd9a5942468d4c32f9f6e902893a34626edb71a
und3f1n3d5 <55402266+und3f1n3d5@users.noreply.github.com>
2022-03-03 14:50:37 +0300

    Update M3i_test.cpp

--- pavliuchenkov_d_m/prj.lab/m3i/M3i_test.cpp
+++ pavliuchenkov_d_m/prj.lab/m3i/M3i_test.cpp
@@ -16,10 +16,10 @@ TEST_CASE("Get/Set") {
     int64_t ptr[8] = {0, 1, 2, 3, 4, 5, 6, 7};
     M3i test(2, 2, 2, ptr);
     CHECK_NOTHROW(test.SetElement(10, 1, 1, 1));
-    CHECK(test.GetElement(1, 1, 1) == 10);
-    CHECK(test.GetElement(0, 0, 0) == 0);
+    CHECK(test.at(1, 1, 1) == 10);
+    CHECK(test.at(0, 0, 0) == 0);
     CHECK_THROWS(test.SetElement(1, 10, 1, 0));
-    CHECK_THROWS(test.GetElement(1, 1, 10));
+    CHECK_THROWS(test.at(1, 1, 10));
 }
 
 TEST_CASE("Resize") {
@@ -28,8 +28,8 @@ TEST_CASE("Resize") {
     CHECK_NOTHROW(test.Resize(1,1,1));
     CHECK(test.GetSize() == 1);
     CHECK_NOTHROW(test.Resize(2, 2, 2));
-    CHECK(test.GetElement(1, 1, 1) == 0);
-    CHECK(test.GetElement(0, 0, 0) == 8);
+    CHECK(test.at(1, 1, 1) == 0);
+    CHECK(test.at(0, 0, 0) == 8);
 }
 
 TEST_CASE("Copy & clone") {
@@ -41,7 +41,7 @@ TEST_CASE("Copy & clone") {
         M3i test_cn = test_cp.clone();
         test_cp.SetElement(2, 0, 0, 0);
         test_cn.SetElement(3, 0, 0, 0);
-        CHECK(test1.GetElement(0, 0, 0) == 2);
+        CHECK(test1.at(0, 0, 0) == 2);
     }
-    CHECK(test1.GetElement(0, 0, 0) == 2);
+    CHECK(test1.at(0, 0, 0) == 2);
 }
